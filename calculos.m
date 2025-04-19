0d900bd540ed93a625d24f1f867f1cddb1c3efd7
Theuzin69 <75169130+Theuzin69@users.noreply.github.com>
2025-04-19 20:40:32 -0300

    Add files via upload

--- calculos.m
+++ calculos.m
@@ -1,12 +1,47 @@
+clc
+
+##Constantes (transitores)
+Beta = 200;
+alpha = Beta / (Beta+1);
+VT = 0.026;
+VA = 100;
+VBE = 0.7;
+
+##Constantes (circuito)
 VCC = 20;
 VEE = -20;
-IEE = 0.001;
-Vin = 0.01;
-VBE = 0.7;
 RL = 100000;
+Vin = 0.01;
+IEE = 0.002;
 
-RI = (VCC - VEE - 2*VBE) / IEE
-AV = gm*Rout / (1 + gm*Re)
+##polarização dos transistores npn
+IE = IEE / 2
+IC = alpha * IE
+IB = IE / (Beta + 1)
 
+####pnp
+##IEpnp = IC;
+##ICpnp = alpha * IEpnp
+##IBpnp = IEpnp / (Beta + 1)
+
+##Pequenos Sinais
+gm = IC / VT
+rpi = VT / IB
+re = VT / IE
+
+##ro
+ro3 = VA / IC
+ro4 = VA / IC
+ro5 = VA / IC
+ro6 = VA / IC
+
+##calculo espelho de corrente
+RI = (VCC - VEE + VBE) / (IEE * (1 + 2/Beta))
+
+##ganho
+ro4ro6 = 1 / (1/ro4 + 1/ro6)
+Rout = 1 / (1/ro4ro6 + 1/RL)
+RE = gm*Rout / (1028*gm) - 1/gm
+AV = gm*Rout / (1 + gm*RE)
 
 
